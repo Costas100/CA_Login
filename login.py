f5e2a26f626e71a60e30b2daa938bc2d46a08138
Costas100 <constantineathanitis@yahoo.com>
2016-10-06 20:10:04 -0400

    started sessions edits

--- login.py
+++ login.py
@@ -1,20 +1,38 @@
 ## Constantine Athanitis
 ## Work 4: Login
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = "RANDOM STRING"
 
 data = open("data/userpass.csv","a")
 data.close()
 
 @app.route("/")
+def root():
+    if "username" in session:
+        return redirect(url_for("/welcome"))
+    else:
+        return redirect(url_for("/frontEnd"))
+
+
+@app.route("/welcome")
+def welc():
+    return render_template("welcome.html")
+
+@app.route("/logout")
+def logout():
+    if "Lo" in request.form["submit"]:
+        session.pop("username")
+        return render.template("logout.html")
+
+@app.route("/logreg")
 def start():
-    print request.headers
     return render_template('frontEnd.html')
 
-@app.route("/authenticate/" , methods=['POST'])
+@app.route("/authenticate" , methods=['POST'])
 def auth():
     usr = request.form["user"]
     psw = request.form["pass"]
@@ -26,10 +44,6 @@ def auth():
 
 
 def reg(User, Pass):
-    '''  return check(User)
-     The following code does what check() should do.
-     However, i am getting a return function error, so i made it part of the reg() code'''
-    
     data = open("data/userpass.csv","r")
     list = data.readlines()
     data.close()
@@ -48,17 +62,6 @@ def reg(User, Pass):
     return render_template("frontEnd.html", m = text)
 
 
-'''def check(User):
-    data = open("data/userpass.csv","r")
-    list = data.readlines()
-    data.close()
-    for i in list:
-        x = i.split(",")
-        if (x[0] == User):
-            text = "Oops! That username already exists. Try another one."
-            return render_template("Msg.html", m=text)
-    return
-'''
 
 def log(User, Pass):        
     data = open("data/userpass.csv","r")
@@ -75,21 +78,16 @@ def log(User, Pass):
     for i in list:
         x = i.split(",")
         x[1] = x[1][:-1]
-        '''Checks
-        print User
-        print Pass
-        print x[0]
-        print x[1]
-        '''
         if (x[0] == User):
             if (x[1] == Pass):
+                session["username"] = User
                 return render_template("Msg.html", m=good)
             else:
                 return render_template("Msg.html", m=badP)
         else:
             if (x[1] == Pass):
                 return render_template("Msg.html", m=badU)
-    return render_template("Msg.html", m=bad)
+    return redirect(url_for("/welcome"))
 
             
 if (__name__) == "__main__":

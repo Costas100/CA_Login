cdc9eea97681892d45f578d1387fe2ac49d3f14e
Costas100 <constantineathanitis@yahoo.com>
2016-10-08 00:55:58 -0400

    added final edits

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, url_for, session, redirect
 import hashlib
 
 app = Flask(__name__)
-app.secret_key = "RANDOM STRING"
+app.secret_key = '\x19ad\xc01lJJ\xfd;v\xad^\xfaa3.\xb1lY\x0c\xde\xfe=\x13\xe46\xe5Q3\xb6M'
 
 data = open("data/userpass.csv","a")
 data.close()
@@ -22,11 +22,11 @@ def root():
 def welc():
     return render_template("welcome.html")
 
-@app.route("/logout/")
+@app.route("/logout/", methods = ['POST'])
 def logout():
-    if "Lo" in request.form["submit"]:
+    if "Logout" in request.form["submit"]:
         session.pop("username")
-        return render.template("logout.html")
+        return render_template("logout.html")
 
 @app.route("/logreg/")
 def start():
@@ -68,26 +68,41 @@ def log(User, Pass):
     list = data.readlines()
     data.close()
     
+    
     good = "You sucessfully logged in!"
-    badP = "Password doesn't match the username. Try again."
-    badU = "Username doesn't match the password. Try again."
-    bad = "Username and password can't be found. Try again."
+  #  badP = "Password doesn't match the username. Try again."
+  #  badU = "Username doesn't match the password. Try again."
+    bad = "Username and password don't match. Try again."
     
     Pass = hashlib.sha224(Pass).hexdigest()
 
+    
     for i in list:
+        
         x = i.split(",")
         x[1] = x[1][:-1]
+   
         if (x[0] == User):
             if (x[1] == Pass):
                 session["username"] = User
                 return render_template("Msg.html", m=good)
+
+            '''
+            If I use the below code to differentiate between
+            login errors, the for loop would never run past the
+            first iteration. Thus, I had to keep the error
+            message as generic as possible.
+
             else:
                 return render_template("Msg.html", m=badP)
         else:
             if (x[1] == Pass):
                 return render_template("Msg.html", m=badU)
-    return redirect(url_for("/welcome"))
+            else:
+                return render_template("Msg.html", m=bad)'''
+            
+    return render_template("Msg.html", m= bad)
+
 
             
 if (__name__) == "__main__":
